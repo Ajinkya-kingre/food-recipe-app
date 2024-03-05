6773cc6f75044fd2fdd5ecc641eedcfbf84c54d3
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-03-05 08:48:42 +0530

    complete the recipe-app

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
@@ -6,6 +7,10 @@ export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
   const [loading, setLoading] = useState(false);
   const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
+
+  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -15,11 +20,12 @@ export default function GlobalState({ children }) {
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
       const data = await res.json();
-    //   console.log(data);
+      //   console.log(data);
       if (data?.data?.recipes) {
         setRecipeList(data?.data?.recipes);
         setLoading(false);
         setSearchParam("");
+        navigate("/");
       }
     } catch (error) {
       console.log(error);
@@ -29,9 +35,37 @@ export default function GlobalState({ children }) {
   }
   console.log(loading, recipeList);
 
+  function handleAddToFavorites(getCurrItem) {
+    console.log(getCurrItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(
+      (item) => item.id === getCurrItem.id
+    );
+
+    if (index === -1) {
+      cpyFavoritesList.push(getCurrItem);
+    } else {
+      cpyFavoritesList.splice(index);
+    }
+
+    setFavoritesList(cpyFavoritesList);
+  }
+
+  console.log(favoritesList, "favroiteList");
+
   return (
     <GlobalContext.Provider
-      value={{ searchParam,loading, recipeList, setSearchParam, handleSubmit }}
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        handleAddToFavorites,
+        favoritesList,
+      }}
     >
       {children}
     </GlobalContext.Provider>
--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,3 +1,73 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../../context";
+
 export default function Details() {
-  return <div>Details tits</div>;
+  const { id } = useParams();
+  const { recipeDetailsData, setRecipeDetailsData, handleAddToFavorites, favoritesList } = useContext(GlobalContext);
+  console.log(id);
+
+  useEffect(() => {
+    async function getRecipeDetailsData() {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      );
+      const data = await res.json();
+
+      console.log(data);
+      if (data?.data) {
+        setRecipeDetailsData(data?.data);
+      }
+    }
+
+    getRecipeDetailsData();
+  }, [id]);
+
+  console.log(recipeDetailsData);
+
+  return (
+    <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <div className="row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl group ">
+          <img
+            src={recipeDetailsData?.recipe?.image_url}
+            alt="recipe img"
+            className="w-full h-full object-cover block group-hover:scale-105 duration-300"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <span className="text-sm text-cyan-700 font-medium">
+          {recipeDetailsData?.recipe?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {recipeDetailsData?.recipe?.title}
+        </h3>
+        <div>
+          <button onClick={()=> handleAddToFavorites(recipeDetailsData?.recipe)}  className="text-sm mt-5 px-8 p-3 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md  text-white bg-black">
+           {
+            favoritesList && favoritesList.length > 0 && favoritesList.findIndex(item => item.id === recipeDetailsData?.recipe?.id) !== -1? "Remove From Favorites" : "Add to Favorites"
+           }
+          </button>
+        </div>
+        <div>
+          <span className="text-2xl font-semibold text-black">
+            Ingredients:
+          </span>
+          <ul className="flex flex-col gap-3">
+            {recipeDetailsData?.recipe?.ingredients.map((ingredient) => (
+              <li>
+                <span className="text-2xl font-semibold text-black">
+                  {ingredient.quantity} {ingredient.unit}
+                </span>
+                <span className="text-2xl font-semibold text-black">
+                  {ingredient.description}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
